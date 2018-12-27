2f7e84d082b25fd7a81b3afa6931cfac3662f1e2
Josh Choo <dev@joshchoo.com>
2018-12-27 10:51:24 +0700

    Add perf and iop blobs from OP5 OOS OB23
    These blobs need to be present in /sytem/lib[64] for QPerformance to
    work.
    
    Signed-off-by: Josh Choo <dev@joshchoo.com>

--- Android.mk
+++ Android.mk
@@ -34,3 +34,59 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libqti-perfd-client_system
+LOCAL_MODULE_OWNER := qti
+LOCAL_SRC_FILES_64 := proprietary/lib64/libqti-perfd-client_system.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libqti-perfd-client_system.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libqti_performance
+LOCAL_MODULE_OWNER := qti
+LOCAL_SRC_FILES_64 := proprietary/lib64/libqti_performance.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libqti_performance.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.qti.hardware.iop@1.0
+LOCAL_MODULE_OWNER := qti
+LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.qti.hardware.iop@1.0.so
+LOCAL_SRC_FILES_32 := proprietary/lib/vendor.qti.hardware.iop@1.0.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.qti.hardware.iop@2.0
+LOCAL_MODULE_OWNER := qti
+LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.qti.hardware.iop@2.0.so
+LOCAL_SRC_FILES_32 := proprietary/lib/vendor.qti.hardware.iop@2.0.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.qti.hardware.perf@1.0
+LOCAL_MODULE_OWNER := qti
+LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.qti.hardware.perf@1.0.so
+LOCAL_SRC_FILES_32 := proprietary/lib/vendor.qti.hardware.perf@1.0.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
