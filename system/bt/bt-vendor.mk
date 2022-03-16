72db9f38eff8e6c2d6610dde81b1e19a8a0451a5
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-16 17:56:42 +0900

    bt: drop dun-server
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- system/bt/bt-vendor.mk
+++ system/bt/bt-vendor.mk
@@ -6,7 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/common/system/bt
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/common/system/bt/proprietary/system_ext/bin/dun-server:$(TARGET_COPY_OUT_SYSTEM_EXT)/bin/dun-server \
     vendor/qcom/common/system/bt/proprietary/system_ext/lib/com.qualcomm.qti.ant@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/com.qualcomm.qti.ant@1.0.so \
     vendor/qcom/common/system/bt/proprietary/system_ext/lib64/com.qualcomm.qti.ant@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/com.qualcomm.qti.ant@1.0.so \
     vendor/qcom/common/system/bt/proprietary/system_ext/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libaptXHD_encoder.so \
